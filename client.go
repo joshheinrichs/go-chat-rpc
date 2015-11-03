46341417a51b97e463d32a85eba15ae7c6333469
Josh Heinrichs <joshiheinrichs@gmail.com>
2015-11-03 00:50:13 -0600

    Add RPC interface and finish client implementation

--- client.go
+++ client.go
@@ -1,33 +1,97 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"net/rpc"
+	"os"
+	"strings"
+	"sync"
 )
 
-// Requests strings from the server and outputs them to stdout.
-func Read() {
+const (
+	CMD_PREFIX = "/"
+	CMD_CREATE = CMD_PREFIX + "create"
+	CMD_LIST   = CMD_PREFIX + "list"
+	CMD_JOIN   = CMD_PREFIX + "join"
+	CMD_LEAVE  = CMD_PREFIX + "leave"
+	CMD_HELP   = CMD_PREFIX + "help"
+	CMD_NAME   = CMD_PREFIX + "name"
+	CMD_QUIT   = CMD_PREFIX + "quit"
+)
 
-}
+var token string
+var client *rpc.Client
+var wg sync.WaitGroup
 
 // Adds strings from stdin to the server.
-func Write() {
+func Input() {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		str, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		Parse(str)
+	}
+}
 
+func Parse(str string) (err error) {
+	switch {
+	default:
+		err = client.Call("Client.SendMessage", &Args{token, str}, nil)
+	case strings.HasPrefix(str, CMD_CREATE):
+		name := strings.TrimSuffix(strings.TrimPrefix(str, CMD_CREATE+" "), "\n")
+		err = client.Call("Client.CreateChatRoom", &Args{token, name}, nil)
+	case strings.HasPrefix(str, CMD_LIST):
+		err = client.Call("Client.ListChatRooms", &token, nil)
+	case strings.HasPrefix(str, CMD_JOIN):
+		name := strings.TrimSuffix(strings.TrimPrefix(str, CMD_JOIN+" "), "\n")
+		err = client.Call("Client.JoinChatRoom", &Args{token, name}, nil)
+	case strings.HasPrefix(str, CMD_LEAVE):
+		err = client.Call("Client.LeaveChatRoom", &token, nil)
+	case strings.HasPrefix(str, CMD_NAME):
+		name := strings.TrimSuffix(strings.TrimPrefix(str, CMD_NAME+" "), "\n")
+		err = client.Call("Client.ChangeName", &Args{token, name}, nil)
+	case strings.HasPrefix(str, CMD_HELP):
+		err = client.Call("Client.Help", &token, nil)
+	case strings.HasPrefix(str, CMD_QUIT):
+		err = client.Call("Client.Quit", &token, nil)
+	}
+	return err
+}
+
+// Requests strings from the server and outputs them to stdout.
+func Output() {
+	for {
+		var message string
+		err := client.Call("Client.ReceiveMessage", &token, &message)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Print(message)
+	}
 }
 
 func main() {
-	client, err := rpc.DialHTTP("tcp", ":1234")
+	wg.Add(1)
+
+	var err error
+	client, err = rpc.DialHTTP("tcp", ":1234")
 	if err != nil {
 		log.Fatal("dialing:", err)
 	}
 
-	// Synchronous call
-	args := &Args{7, 8}
-	var reply int
-	err = client.Call("Arith.Multiply", args, &reply)
+	err = client.Call("Client.Connect", struct{}{}, &token)
 	if err != nil {
-		log.Fatal("arith error:", err)
+		log.Fatal("connection error:", err)
 	}
-	fmt.Printf("Arith: %d*%d=%d\n", args.A, args.B, reply)
+	fmt.Printf("token: %s\n", token)
+
+	go Input()
+	go Output()
+
+	wg.Wait()
 }
--- server.go
+++ server.go
@@ -1,15 +1,100 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"log"
 	"net"
 	"net/http"
 	"net/rpc"
 )
 
+var users map[string]*User = make(map[string]*User)
+
+type Client int
+
+func (t *Client) Connect(args *struct{}, token *string) error {
+	log.Println("Connect")
+	*token = randomString(64)
+	user := NewUser(*token)
+	users[*token] = user
+	return nil
+}
+
+func (t *Client) SendMessage(args *Args, _ *struct{}) error {
+	log.Println("SendMessage")
+	user := users[args.Token]
+	user.Outgoing <- args.String
+	log.Print(args.String)
+	return nil
+}
+
+func (t *Client) CreateChatRoom(args *Args, _ *struct{}) error {
+	log.Println("CreateChatRoom")
+	return nil
+}
+
+func (t *Client) ListChatRooms(args *string, _ *struct{}) error {
+	log.Println("ListChatRooms")
+	return nil
+}
+
+func (t *Client) JoinChatRoom(args *Args, _ *struct{}) error {
+	log.Println("JoinChatRoom")
+	return nil
+}
+
+func (t *Client) LeaveChatRoom(token *string, _ *struct{}) error {
+	log.Println("LeaveChatRoom")
+	return nil
+}
+
+func (t *Client) ChangeName(args *Args, _ *struct{}) error {
+	log.Println("ChangeName")
+	return nil
+}
+
+func (t *Client) Help(token *string, _ *struct{}) error {
+	log.Println("Help")
+	return nil
+}
+
+func (t *Client) Quit(token *string, _ *struct{}) error {
+	log.Println("Quit")
+	return nil
+}
+
+func (t *Client) ReceiveMessage(token *string, message *string) error {
+	log.Println("ReceiveMessage")
+	user := users[*token]
+	*message = <-user.Outgoing
+	return nil
+}
+
+type User struct {
+	Token    string
+	Name     string
+	Outgoing chan string
+}
+
+func NewUser(token string) *User {
+	return &User{
+		Token:    token,
+		Name:     "Anonymous",
+		Outgoing: make(chan string),
+	}
+}
+
+// randomString returns a random string with the specified length
+func randomString(length int) (str string) {
+	b := make([]byte, length)
+	rand.Read(b)
+	return base64.StdEncoding.EncodeToString(b)
+}
+
 func main() {
-	arith := new(Arith)
-	rpc.Register(arith)
+	client := new(Client)
+	rpc.Register(client)
 	rpc.HandleHTTP()
 	l, e := net.Listen("tcp", ":1234")
 	if e != nil {
